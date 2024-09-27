eb00a5553cf3bfe65e085e8b52c25d147f99e9f2
bono43 <bono43@nexondv.com>
2024-09-27 17:30:06 +0700

    feat: add

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,22 @@
 <script>
-	export let name;
+	export let name = "Bon";
+	export let age = 27;
+
+	$: uppercaseName = name.toUpperCase();
+
+	$: console.log(name);
+
+	$: if (name === "Bonnie") {
+		age = 55;
+	}
+	
+	const changeAge = () => {
+		age++;
+	};
+
+	const changeName = () => {
+		name = "Bonnie";
+	};
 </script>
 
 <style>
@@ -8,4 +25,6 @@
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Hello {uppercaseName}! {age}</h1>
+<button on:click={changeAge}>Change Age</button>
+<button on:click={changeName}>Change name</button>
